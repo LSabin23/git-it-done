fc4ca9f16156734f0fa6417738be4a8de877ca8e
unknown <44147809+LSabin23@users.noreply.github.com>
2021-11-03 20:53:36 -0400

    added error handling for invalid username, users without repos, and no network connection

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -7,11 +7,20 @@ var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = 'https://api.github.com/users/' + user + '/repos'
   // make a request to the url
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user)
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data, user)
+        })
+      } else {
+        alert('Error: GitHub user not found.')
+      }
     })
-  })
+    .catch(function (error) {
+      // Notice this .catch() getting chained onto the end of the .then()
+      alert('Unable to connect to GitHub.')
+    })
 }
 
 var formSubmitHandler = function (event) {
@@ -28,6 +37,12 @@ var formSubmitHandler = function (event) {
 }
 
 var displayRepos = function (repos, searchTerm) {
+  // check if API returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = 'No repositories found.'
+    return
+  }
+
   repoContainerEl.textContent = ''
   repoSearchTerm.textContent = searchTerm
 
